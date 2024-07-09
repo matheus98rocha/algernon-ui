d9a3ff8cf556a98efbbd23ce2df73509693fbe32
matheus98rocha <matheus98rocha@gmail.com>
2024-07-09 20:06:03 -0300

    feat - form login and signup

--- shoppy-ui/app/page.tsx
+++ shoppy-ui/app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
+      <h1>In development</h1>
     </main>
   );
 }
