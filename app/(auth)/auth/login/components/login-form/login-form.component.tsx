8de8cebd113b13699d70468a06cd879fc9d2021f
matheus98rocha <matheus98rocha@gmail.com>
2024-07-22 17:27:07 -0300

    feat: ajustando texto de manter dados

--- app/(auth)/auth/login/components/login-form/login-form.component.tsx
+++ app/(auth)/auth/login/components/login-form/login-form.component.tsx
@@ -59,7 +59,7 @@ function LoginForm() {
         {"Entrar"}
       </LoadingButton>
       <Box gap={1} className="flex items-center justify-between pt-2">
-        <FormControlLabel control={<Checkbox />} label="Continuar conectado" />
+        <FormControlLabel control={<Checkbox />} label="Manter dados" />
         <Link component={Link} href="/auth/forgot" color={"#1877F2"}>
           {"Esqueceu sua senha?"}
         </Link>
