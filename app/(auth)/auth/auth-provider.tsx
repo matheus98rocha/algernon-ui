8086abefc4b7d28f735d36eb32ad1687aec94851
matheus98rocha <matheus98rocha@gmail.com>
2024-08-11 23:54:51 -0300

    feat: ajustes componente layout, passando para server component

--- app/(auth)/auth/auth-provider.tsx
+++ app/(auth)/auth/auth-provider.tsx
@@ -1,16 +1,44 @@
 "use client";
 
 import DefaultProgressBar from "@/app/common/components/default-progress-bar/default-progress-bar.component";
+import Image from "next/image";
+import AlgernonLogo from "../../assets/logo-algernon.png";
+import { Box, Grow, useMediaQuery, useTheme } from "@mui/material";
 
 type ProvidersProps = {
   children: React.ReactNode;
 };
 
 export default function AuthProviders({ children }: ProvidersProps) {
+  const theme = useTheme();
+  const onlyMediaScreen = useMediaQuery(theme.breakpoints.up("md"));
   return (
     <>
-      <DefaultProgressBar />
-      {children}
+      <Grow in={true}>
+        <Box
+          sx={{
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "background.default",
+            gap: 10,
+          }}
+        >
+          {onlyMediaScreen && (
+            <Image
+              src={AlgernonLogo}
+              alt="algernon-logo"
+              priority
+              placeholder="blur"
+              sizes="( max-width: 768px ) 100vw, ( max-width: 1200px ) 50vw, 33vw"
+              loading="eager"
+            />
+          )}
+          <DefaultProgressBar />
+          {children}
+        </Box>
+      </Grow>
     </>
   );
 }
--- app/(auth)/auth/layout.tsx
+++ app/(auth)/auth/layout.tsx
@@ -1,8 +1,3 @@
-"use client";
-import { Box, Grow, useMediaQuery, useTheme } from "@mui/material";
-import Image from "next/image";
-
-import AlgernonLogo from "../../assets/logo-algernon.png";
 import AuthProviders from "./auth-provider";
 
 export default function AuthLayout({
@@ -10,33 +5,9 @@ export default function AuthLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const theme = useTheme();
-  const onlyMediaScreen = useMediaQuery(theme.breakpoints.up("md"));
-
   return (
-    <Grow in={true}>
-      <Box
-        sx={{
-          height: "100vh",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "background.default",
-          gap: 10,
-        }}
-      >
-        {onlyMediaScreen && (
-          <Image
-            src={AlgernonLogo}
-            alt="algernon-logo"
-            priority
-            placeholder="blur"
-            sizes="( max-width: 768px ) 100vw, ( max-width: 1200px ) 50vw, 33vw"
-            loading="eager"
-          />
-        )}
-        <AuthProviders>{children}</AuthProviders>
-      </Box>
-    </Grow>
+    <>
+      <AuthProviders>{children}</AuthProviders>
+    </>
   );
 }
